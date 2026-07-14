93d865dc15628b28425025fc015e507ed7fb5dd0
bhawana <jainbhawana245@gmail.com>
2026-07-14 20:55:08 +0530

    [accounts] Add user registration, JWT auth, profile API and templates

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('role', 'bio', 'avatar', 'github_url', 'linkedin_url', 'email_verified')}),
+    )
+    list_display = ['username', 'email', 'role', 'is_staff', 'date_joined']
+    list_filter = ['role', 'is_staff', 'is_superuser', 'email_verified']
 # Register your models here.
